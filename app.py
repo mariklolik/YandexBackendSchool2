a42d8b19024bc80a2828db388e21e5b0a7595ddd
Mr.President <41217710+mariklolik@users.noreply.github.com>
2022-09-15 11:33:50 +0700

    Changed app.py in order to start it with gunicorn

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from dbremote import db_session
 from flask import Flask
-import os
 import api_new
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "sk"
 UPLOAD_FOLDER = '/data'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-def main():
-    db_session.global_init("db/data.sqlite")
-    port = int(os.environ.get("PORT", 80))
-    app.register_blueprint(api_new.blueprint)
-    app.run(port=port, host='0.0.0.0', debug=True)
-    return 0
-
-
+db_session.global_init("db/data.sqlite")
+app.register_blueprint(api_new.blueprint)
